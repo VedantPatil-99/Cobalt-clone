622a46903c2c282d640bbc3269d0e3eacedcb508
VedantPatil-99 <vedantpatil9977@gmail.com>
2025-10-04 19:36:53 +0530

    Add GradientBg component and update GridPattern class for consistency

---
+++ src/components/GradientBg.tsx
@@ -0,0 +1,9 @@
+const GradientBg = () => {
+  return (
+    <>
+      <div className="pointer-events-none absolute inset-0 -z-10 h-full bg-[image:radial-gradient(80%_90%_at_50%_-20%,hsl(206,81.9%,65.3%,0.4),rgba(255,255,255,0))]"></div>
+    </>
+  );
+};
+
+export default GradientBg;
